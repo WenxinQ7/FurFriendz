f7987b012d2eccc13bc96c2c61ae25263c192ae0
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-10-11 06:27:23 +0000

    finish CRUD operations

--- module/userCollection.js
+++ module/userCollection.js
@@ -3,38 +3,6 @@ const url =
   "mongodb+srv://qiwenxin98:Zjjxwjp@cluster0.chnfjby.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
-// async function User() {
-//   try {
-//     // Connect to the MongoDB server
-//     await client.connect();
-
-//     const db = client.db("FurFriendz");
-//     const collection = db.collection("users");
-
-//     const UserSchema = {
-//       username: { type: String, required: true, unique: true },
-//       password: { type: String, required: true },
-//     };
-
-//     const model = {
-//       create: async (user) => {
-//         const result = await collection.insertOne(user);
-//         return result.ops[0];
-//       },
-//       findUserByUsername: async (username) => {
-//         return collection.findOne({ username });
-//       },
-//     };
-
-//     return model;
-//   } catch (err) {
-//     console.error("Error connecting to MongoDB:", err);
-//     throw err;
-//   }
-// }
-
-// module.exports = User;
-
 async function connect() {
   try {
     await client.connect();
@@ -55,7 +23,7 @@ async function createUser(user) {
 async function findUserByUsername(username) {
   const db = await connect();
   // console.log(username);
-  return db.collection("users").findOne({ username });
+  return db.collection("users").findOne(username);
 }
 
 module.exports = {
