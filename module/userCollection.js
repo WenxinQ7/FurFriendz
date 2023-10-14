5e697db3b0013748c571a3cc64b1433529f2d9e8
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-10-14 23:56:59 +0000

    fix index page

--- module/userCollection.js
+++ module/userCollection.js
@@ -1,16 +1,15 @@
-const db  = require("./db").client;
-
+const db = require("./db").client;
 
 // Create a new user document
 async function createUser(user) {
-  const result = await db.collection("users").insertOne(user);
+  const result = await db.db("FurFriendz").collection("users").insertOne(user);
   return result.insertedId;
 }
 
 // Find a user by their username
 async function findUserByUsername(username) {
   // console.log(username);
-  return db.collection("users").findOne(username);
+  return db.db("FurFriendz").collection("users").findOne(username);
 }
 
 module.exports = {
